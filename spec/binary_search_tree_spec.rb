09017d9dcb8c77042e7acb6fbc8de82f09e01dd0
Chrissie Deist <chrissie.deist@gmail.com>
2014-05-06 15:45:57 -0700

    Create tests fpr BinaryNode

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,22 +1,57 @@
 require 'rspec'
-require 'card'
+require 'binary_search_tree'
 
-describe Card do
-  subject(:card) { Card.new(:diamond, :queen) }
-  its(:suit) { should be (:diamond) }
-  its(:value) { should eql (:queen) }
+describe BinaryNode do
+  subject(:node) { BinaryNode.new(5, 5, nil) }
+  # its(:suit) { should be (:diamond) }
+  # its(:value) { should eql (:queen) }
 
-  it "tells you its ranking" do
-    expect(card.rank).to eql(12)
+  describe "#is_leaf?" do
+    it "returns true when height is 0" do
+      expect(node.is_leaf?).to be_true
+    end
+
+    it "returns false when height is not 0" do
+      node.height = 1
+      expect(node.is_leaf?).to be_false
+    end
   end
 
-  let(:other_card) { Card.new(:spade, :king) }
-  let(:same_val_card) { Card.new(:spade, :queen) }
+  describe "#max_children_height" do
 
-  # let(:other_card) { double('other_card', :rank => 13 ) }
-  it "can compare it's rank to other cards ranks" do
-    expect(card > (other_card)).to be_false
+    before(:each) do
+      @left = double('left_child', :present? => true, :height => 3)
+      @right = double('right_child', :present? => true, :height => 2)
+      node.left, node.right = @left, @right
+    end
+    context "when both children are present and the left has a greater height" do
+      it "returns the value of the left child's height" do
+        expect(node.max_children_height).to eql(3)
+      end
+    end
+
+    context "when both children are present and right has a greater height" do
+      it "returns the value of the right child's height" do
+        @right.stub(:height).and_return(4)
+        expect(node.max_children_height).to eql(4)
+      end
+    end
+
+    context "when only the left child is present" do
+      it "returns the value of the left child's height" do
+        @right.stub(:present?).and_return(false)
+        expect(node.max_children_height).to eql(3)
+      end
+    end
+
+    context "when only the right child is present" do
+      it "returns the value of the right child's height" do
+        @left.stub(:present?).and_return(false)
+        expect(node.max_children_height).to eql(2)
+      end
+    end
   end
+
 
 
 
